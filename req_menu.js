496deef66e7ef1d7d9b6ae0026ad4a819633c912
PALMIER_ROBIN <PALMIER_ROBIN@intech-s1>
2021-04-28 12:43:30 +0200

    req menu

---
+++ req_menu.js
@@ -0,0 +1,27 @@
+//=========================================================================
+// Traitement de "req_commencer"
+// Auteurs : P. Thiré & T. Kerbrat
+// Version : 15/09/2020
+//=========================================================================
+"use strict";
+
+const fs = require("fs");
+require('remedial');
+
+const trait = function (req, res, query) {
+
+	let marqueurs;
+	let page;
+
+	// AFFICHAGE DE LA PAGE D'ACCUEIL
+
+	page = fs.readFileSync('m_menu.html', 'utf-8');
+
+
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write(page);
+	res.end();
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
