6148715ee9bc8ea053b9d6256db96cb20b6db881
ORIEUX <ORIEUX@intech-s1>
2021-04-30 10:14:27 +0200

    début de map : affichage de points

--- test/map.js
+++ test/map.js
@@ -1,11 +1,36 @@
 "use strict"
 
 let grille = [];
-let L,l;
-while ( grille) {
-	for (y = 1; y <= 15; y++) {
-		if () {
-		process.stdout.write (". ");
-		}
-	}
+let l = 0, h = 0; //largeur / hauteur  
+
+for(let i = 0;i < 10;i++) {
+	grille[i] = new Array(20);
+	//console.log(1);
 }
+
+//console.log(grille);
+
+while (h < 20) {
+	l = 0;
+	while(l < 10){
+		grille[l][h] = ". ";
+		l++;
+		//console.log(2);
+	}
+	h++;
+}
+
+h = 0;
+l = 0; 
+
+while (h < 20) {
+	l = 0;
+	while(l < 10){
+		process.stdout.write(grille[l][h]);
+		l++;
+		//console.log(3);
+	}
+	console.log();
+	h++;
+}
+
