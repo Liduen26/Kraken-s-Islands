486e42f8bcff30422231bdd581a94413216437c8
ORIEUX <ORIEUX@intech-s1>
2021-05-04 11:29:32 +0200

    ajout d'un début de carte dessinée

--- test/map2.js
+++ test/map2.js
@@ -7,36 +7,51 @@ let y = 0, x = 0; // hauteur / largeur
 let seed;
 let elevation;
 
+const colors = require("colors/safe");
+const kbd = require("kbd");
+
 const SimplexNoise = require("simplex-noise");
-let simplex = new SimplexNoise("kraken");
+let simplex = new SimplexNoise("kraken2");
 const hauteur = 30;
 const largeur = 15;
+let zoom = 10;
+let x_p, y_p;
 
-console.log(seed = Math.floor(Math.random() * 100000) + 1);
-
+/*
 for(let y = 0;y < hauteur;y++) {
 	carte.push([]);
 
 	for(let x = 0;x < largeur;x++) {
-		carte[y].push(". ");
+		carte[y].push();
 	}
 }
 y = 0;
 x = 0;
+*/
 
 while (y < 30) {
 	x = 0;
 	while(x < 15){
-		process.stdout.write(carte[y][x]);
+		//process.stdout.write(carte[y][x]);
+		x_p = x / zoom;
+		y_p = y / zoom;
+		elevation = simplex.noise2D(x_p, y_p);
+		//elevation = elevation.toFixed(2);
 		
-		elevation = simplex.noise2D(x, y);
-		elevation = String(elevation);
-		process.stdout.write(elevation);
+		if(elevation < 0.5) {
+			process.stdout.write(colors.bgBlue("  "));
+		}else{
+			process.stdout.write(colors.bgGreen("  "));
 
-		x++;
+		}
+		//process.stdout.write(colors.bgGreen.green(elevation + "  " ));
+
+	x++;
 		//console.log(3);
 	}
 	console.log();
 	y++;
 }
- 
+kbd.getLineSync();
+
+
