4cc3c3f9e184ebb8a21dc709b73b0e3bb47b32a2
ORIEUX <ORIEUX@intech-s1>
2021-05-07 16:07:01 +0200

    modif des nom de la req d'attente

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const req_inscrire = require("./req_inscrire.js");
 const req_menu = require("./req_menu.js");
 const req_afficher_creation = require("./req_afficher_creation.js");
 const req_afficher_rejoindre = require("./req_afficher_rejoindre.js");
-const req_afficher_attente = require("./req_afficher_attente.js");
+const req_creer = require("./req_creer.js");
 
 const req_statique = require("./req_statique.js");
 const req_erreur = require("./req_erreur.js");
@@ -78,7 +78,7 @@ const traite_requete = function (req, res) {
 				req_afficher_rejoindre(req, res);
 				break;
 			case '/req_creer':
-				req_afficher_attente(req, res);
+				req_creer(req, res);
 				break;
 
 			default:
