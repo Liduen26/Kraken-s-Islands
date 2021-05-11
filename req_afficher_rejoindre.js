3185d0edf913ccec89ca5364e587b66aad605bd0
ORIEUX <ORIEUX@intech-s1>
2021-05-11 10:17:56 +0200

    ajout du début de aff_rejoindre.js

--- req_afficher_rejoindre.js
+++ req_afficher_rejoindre.js
@@ -12,14 +12,21 @@ const trait = function (req, res, query) {
 
 	let marqueurs;
 	let page;
+	let content_rep = [];
 
 	// AFFICHAGE DE LA modele_formulaire_inscription
 
 	page = fs.readFileSync('m_rejoindre_partie.html', 'utf-8');
 
+	content_rep = fs.readdirSync("./partie");
+	console.log(content_rep);
+
+	for(let i = 0;i < content_rep.length;i++) {
+		//ici faut mettre un truc pour générer de l'html et le join à la fin
+	}
+
 	marqueurs = {};
-	marqueurs.erreur = "";
-	marqueurs.pseudo = "";
+	marqueurs.parties = content_rep[0];
 	page = page.supplant(marqueurs);
 
 	res.writeHead(200, { 'Content-Type': 'text/html' });
