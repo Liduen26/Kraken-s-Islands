b9fd08908a68291b4b6c797a29750adac73d2d1c
PALMIER_ROBIN <PALMIER_ROBIN@intech-s1>
2021-06-07 11:51:56 +0200

    salle tour suivant

---
+++ req_tour_suivant2.js
@@ -0,0 +1,21 @@
+//module d'actualisation de la salle d'attente 
+
+"use strict"
+
+const fs = require("fs");
+let page;
+
+function attente(req, res, query) {
+
+page = fs.readFileSync("./m_tour_suivant.html", "UTF-8");
+page = page.supplant(marqueurs);
+
+  res.writeHead(200, { "Content-Type": "text/html"});
+    res.write(page);
+    res.end();
+}
+
+module.exports = attente;
+
+
+
