9ae03b310a0195b879b79124c1053ebdf1bb5ed6
ORIEUX <ORIEUX@intech-s1>
2021-06-11 11:30:21 +0200

    fin du req_tir

--- req_tir.js
+++ req_tir.js
@@ -22,12 +22,16 @@ function tir(req, res, query) {
 	page = fs.readFileSync("m_jeu.html", "UTF-8");
 
 	parties = JSON.parse(fs.readFileSync("./index_parties.json", "UTF-8"));
-	
 	//découpage des co de tir
-	for(i = 0;i < query.tir.length; i++) {
-		coord_tir[i] = [];
-		coord_tir[i] = query.tir[i].split("_");
-		
+	if(Array.isArray(query.tir) === true) {
+		for(i = 0;i < query.tir.length; i++) {
+			coord_tir[i] = [];
+			coord_tir[i] = query.tir[i].split("_");
+			
+		}
+	} else {
+		coord_tir[0] = [];
+		coord_tir[0] = query.tir.split("_");
 	}
 
 	//récupération du nom de l'autre joueur
@@ -43,10 +47,13 @@ function tir(req, res, query) {
 
 	sauvegarde = JSON.parse(fs.readFileSync(`./partie/${query.nom_partie}.json`, "UTF-8"));
 	for(i = 0;i < coord_tir.length; i++) {
-		if(sauvegarde[player_autre].coordonnees.y === coord_tir[i][0] && sauvegarde[player_autre].coordonnees.x === coord_tir[i][1]) {
+		if(sauvegarde[player_autre].coordonnees.y === Number(coord_tir[i][0]) && sauvegarde[player_autre].coordonnees.x === Number(coord_tir[i][1])) {
 			sauvegarde[player_autre].stats.pv -= sauvegarde[req.headers.cookie].stats.atq;
+			//notes : il rentre pas dans la boucle jsp pk
 		}
 	}
+
+	fs.writeFileSync(`./partie/${query.nom_partie}.json`, JSON.stringify(sauvegarde), "UTF-8");
 
 
 	mod_aff(req, res, page, query.nom_partie);
